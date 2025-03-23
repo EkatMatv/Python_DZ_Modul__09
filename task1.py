83b95cba2d00853681e92a12d88178b0d0d05777
Ekat Matv <ekatmatv@example.com>
2025-03-23 22:51:42 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,19 @@
+'''Дано два текстовых файла. Выяснить, совпадают ли
+их строки. Если нет, то вывести несовпадающую строку
+из каждого файла.'''
+
+with open('text1.txt', 'r', encoding='utf-8') as file:
+    lines1 = file.readlines()
+with open('text2.txt', 'r', encoding='utf-8') as file:
+    lines2 = file.readlines()
+max_lines = max(len(lines1), len(lines2))
+if lines1 == lines2:
+    print('Все строки в файлах совпадают')
+for i in range(max_lines):
+    line1 = lines1[i] if i < len(lines1) else ' '
+    line2 = lines2[i] if i < len(lines2) else ' '
+    if line1 != line2:
+         print(f"Несовпадение в строке {i + 1}:")
+         print(f'Файл 1: {line1}')
+         print(f'Файл 2: {line2}')
+
